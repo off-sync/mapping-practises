724ba0bba53e6a3e124be5e4fc531fe15d529e34
Bas Verweij <bas@off-sync.com>
2019-05-25 18:14:19 +0200

    Updated project info and license/copyright.

--- src/OffSync.Mapping.Practises/IMapper.cs
+++ src/OffSync.Mapping.Practises/IMapper.cs
@@ -1,4 +1,9 @@
-﻿namespace OffSync.Mapping.Practises
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Off-Sync.com. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+namespace OffSync.Mapping.Practises
 {
     public interface IMapper<in TSource, out TTarget>
     {
